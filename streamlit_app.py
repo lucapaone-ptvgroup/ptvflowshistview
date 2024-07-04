7a3737c4e02d41d99bc15a4c468caeca7c2c1c13
lucapaone-ptvgroup <71133453+lucapaone-ptvgroup@users.noreply.github.com>
2024-07-04 15:01:13 +0200

    revereted to 6 days ago

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,31 @@
-# my_app.py
 import streamlit as st
 import pandas as pd
 import requests
 import logging
-
+from datetime import datetime, timedelta
 # Enable logging
 logging.basicConfig(level=logging.DEBUG)
-
 API_KEY = "put here your API key"
 # Define the base URLs for the different API endpoints
 KPI_ENG_URL = "https://api.myptv.com/kpieng/v1/instance/all"
 KPI_HISTORICAL_URL = "https://api.myptv.com/kpistats/v1/historical/result/by-kpi-id"
 KPI_24HOURS_URL = "https://api.myptv.com/kpieng/v1/result/by-kpi-id"
-
-
 # Define headers for the API requests
 HEADERS = {
     "apiKey": API_KEY,
     "Accept": "*/*",
     "Connection": "keep-alive"
 }
-
 def extract_timetostart(param_dict):
     try:
         return param_dict.get('parameters', {}).get('timetostart', None)
     except AttributeError:
         return None
-
 # Fetch all KPI definitions
 def fetch_all_kpis(api_key):
     try:
         print("Fetching all KPI definitions...")
-        HEADERS["apiKey"]=api_key
+        HEADERS["apiKey"] = api_key
         response = requests.get(KPI_ENG_URL, headers=HEADERS)
         response.raise_for_status()  # Raise an exception if the response status code is not 200
         kpi_data = response.json()
@@ -43,7 +37,6 @@ def fetch_all_kpis(api_key):
     except requests.RequestException as e:
         st.error(f"Error fetching data: {e}")
         return None
-
 # Fetch last 24 hours data for a specific KPI
 def fetch_last_24_hours_data(kpi_id):
     print(f"Fetching last 24 hours data for KPI: {kpi_id}")
@@ -60,7 +53,6 @@ def fetch_last_24_hours_data(kpi_id):
     else:
         print(f"Failed to fetch data for KPI: {kpi_id}, Status Code: {response.status_code}, Response: {response.text}")
     return pd.DataFrame()
-
 # Fetch historical stats for a specific KPI
 def fetch_historical_stats(kpi_id):
     print(f"Fetching historical stats for KPI: {kpi_id}")
@@ -83,86 +75,106 @@ def fetch_historical_stats(kpi_id):
     else:
         print(f"Failed to fetch historical stats for KPI: {kpi_id}, Status Code: {response.status_code}, Response: {response.text}")
     return pd.DataFrame()
-
 # Round datetime to the nearest 5-minute bucket
 def round_to_nearest_5min(timestamp):
     dt = datetime.fromisoformat(timestamp.replace('Z', '+00:00'))
     new_minute = (dt.minute // 5) * 5
     return dt.replace(minute=new_minute, second=0, microsecond=0)
-
-
 def main():
     data = None
-    st.title("API Data Table")
-
+    st.title("PTV FLOWS data analysis")
     # Text input for API key
-    api_key = st.text_input("Enter your API key:")
-
+    api_key_input = st.text_input("Enter your API key:")
+    fetch_KPIdef_button = st.button("Fetch KPI definitions and last data")
+    loading_txt = st.empty()
+    loading_txt.text("...")
     # Button to fetch data
-    fetchKPIdef = st.button("Fetch KPI definitions")
-
-    if fetchKPIdef:
-        if api_key:
+    if fetch_KPIdef_button:
+        if api_key_input:
             # Fetch data from the API
-            API_KEY = api_key
-            kpi_ids_df = fetch_all_kpis(api_key)
-
+            API_KEY = api_key_input
+            kpi_ids_df = fetch_all_kpis(api_key_input)
             if kpi_ids_df is not None:
-
-                                #st.write(data.dtypes)
                 # Display the data in a table
-                kpi_ids_df_gui = st.dataframe(kpi_ids_df)
-                more_data_btn = st.button("Load last 24 hours!")
-                if more_data_btn:
-                    #print (kpi_ids_df)
-                    kpi_ids =     kpi_ids_df['kpiId']
+                st.dataframe(kpi_ids_df)
+                loading_txt.text("Loading last 24 hours data...")
+                kpi_ids = kpi_ids_df['kpiId']
+                last_24_hours_data = pd.DataFrame()
+                for kpi_id in kpi_ids:
+                    loading_txt.text(f"Fetching data for KPI ID: {kpi_id}")
+                    data = fetch_last_24_hours_data(kpi_id)
+                    last_24_hours_data = pd.concat([last_24_hours_data, data], ignore_index=True)
+                if 'results' in last_24_hours_data.columns:
+                    last_24_hours_data.drop(columns=['results'], inplace=True)
+                print(f"Columns in last_24_hours_data: {last_24_hours_data.columns.tolist()}")
+                # Merge to include 'timetostart' in last_24_hours_data
+                merged_data = last_24_hours_data.merge(kpi_ids_df[['kpiId', 'timetostart']], on='kpiId', how='left')
+                # Check the merged data
+                print("Merged DataFrame with 'timetostart':")
+                print(merged_data.head())
+                # Now you can use the 'timetostart' in your calculations
+                # For example, adding 'timetostart' to 'RoundedTimeStamp'
+                merged_data['ForecastedTimestamp'] = merged_data.apply(
+                    lambda row: row['RoundedTimeStamp'] + timedelta(seconds=row['timetostart']), axis=1
+                )
+                last_24_hours_data = merged_data
+                print("Last 24 hours data head:")
+                print(last_24_hours_data.head())      
+                # Display additional data tables
+                st.write("Last 24 Hours Data:")
+                st.dataframe(last_24_hours_data)
+                historical_stats_data = pd.DataFrame()
+                for kpi_id in kpi_ids:
+                    loading_txt.text(f"Fetching HIST data for KPI ID: {kpi_id}")
+                    data = fetch_historical_stats(kpi_id)
+                    if(data is not None):
+                        historical_stats_data = pd.concat([historical_stats_data, data], ignore_index=True)
+                
+                st.write("Historical Stats Data:")
+                st.dataframe(historical_stats_data)
+                # Group historical data and create groupedHistoricalData
+                groupedHistoricalData = historical_stats_data.groupby(
+                    ['kpiId', 'RoundedTimeStamp']
+                ).agg(
+                    {
+                        'defaultValue': 'sum',
+                        'value': 'sum',
+                        'averageValue': 'sum',
+                        'unusualValue': 'sum',
+                        'progressive': 'max',
+                        'timeStamp' : 'max'
+                    }
+                ).reset_index()
 
-                    last_24_hours_data = pd.DataFrame()
-                    loading = st.write("...")
-                    '''
-                    for kpi_id in kpi_ids:
-                        loading.write(f"kpi_id {kpi_id}")
-                        data = fetch_last_24_hours_data(kpi_id)
-                        last_24_hours_data = pd.concat([last_24_hours_data, data], ignore_index=True)
-
-                    if 'results' in last_24_hours_data.columns:
-                        #print("Dropping 'results' column from last 24 hours data.")
-                        last_24_hours_data.drop(columns=['results'], inplace=True)
-
-                    print(f"Columns in last_24_hours_data: {last_24_hours_data.columns.tolist()}")
-
-                    # Merge to include 'timetostart' in last_24_hours_data
-                    merged_data = last_24_hours_data.merge(kpi_ids_df[['kpiId', 'timetostart']], on='kpiId', how='left')
-
-                    # Check the merged data
-                    print("Merged DataFrame with 'timetostart':")
-                    print(merged_data.head())
-
-                    # Now you can use the 'timetostart' in your calculations
-                    # For example, adding 'timetostart' to 'RoundedTimeStamp'
-                    merged_data['ForecastedTimestamp'] = merged_data.apply(
-                        lambda row: row['RoundedTimeStamp'] + timedelta(seconds=row['timetostart']) , axis=1
+                st.write("Grouped Historical Stats Data:")
+                st.dataframe(groupedHistoricalData)
+                loading_txt.empty()  # Remove the loading text
+                
+                comparison = None
+                # Compare quality of forecasted data with historical data
+                if not last_24_hours_data.empty and not groupedHistoricalData.empty:
+                    print("Comparing forecasted data with historical data...")
+                    # Perform inner join on 'kpiId' and 'ForecastedTimestamp' from merged_data with 'kpiId' and 'RoundedTimeStamp' from groupedHistoricalData
+                    comparison = pd.merge(
+                        last_24_hours_data, 
+                        groupedHistoricalData, 
+                        left_on=['kpiId', 'ForecastedTimestamp', 'overallResult.progressive'], 
+                        right_on=['kpiId', 'RoundedTimeStamp', 'progressive'], 
+                        how='inner'
                     )
-
-                    last_24_hours_data = merged_data
-                    print("Last 24 hours data head:")
-                    print(last_24_hours_data.head())      
-      
-
-                    # Display additional data tables
-                    st.write("Last 24 Hours Data:")
-                    st.dataframe(last_24_hours_data)
-
-                    historical_stats_data = fetch_historical_stats(kpi_id)
-                    st.write("Historical Stats Data:")
-                    st.dataframe(historical_stats_data)
-                    '''
+                    comparison['AbsDelta'] = (comparison['overallResult.value'] - comparison['value']).abs()
+                    comparison['ErrorPerc'] = (comparison['AbsDelta'] / comparison['value']) * 100
+                    # Merge to include 'name' from kpi_ids_df in comparison
+                    comparison = pd.merge(comparison, kpi_ids_df[['kpiId', 'name']], on='kpiId', how='left')
+                    print("Comparison Results:")
+                    print(comparison.head())
+                    st.write("Comparison Results:")
+                    st.dataframe(comparison)
                 else:
-                    st.warning("Nodata?")
+                    warning = "No data available for comparison" 
+                    print(warning)
+                    st.warning(warning)
         else:
             st.warning("Please enter an API key.")
-    else:
-            st.warning("Please PRESS.")
 if __name__ == "__main__":
     main()
-
